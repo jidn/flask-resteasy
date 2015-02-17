4e7b8f29d89de715bd85981d9885953d1e966417
Clinton James <clinton.james@anuit.com>
2015-02-16 21:27:51 -0600

    Prepare setup.py for PyPi
    I want this on Pypi so lets get it ready.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import sys
 from flask_resteasy import __version__
 
 requirements = [
@@ -10,16 +9,27 @@ requirements = [
 
 setup(
     name='Flask-RESTeasy',
-    version=__version__,
-    url='https://www.github.com/jidn/flask-restful/',
     author='Clinton James',
     author_email='clinton.james@anuit.com',
+    url='https://www.github.com/jidn/flask-restful/',
+    download_url='https://github.com/jidn/flask-resteasy/tarball/0.0.1.dev',
     description='Create easy REST APIs with Flask',
     long_description=open('README.md').read(),
-    packages=find_packages(exclude=['tests']),
+    version=__version__,
+    keywords=['flask', 'REST'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+    ],
+    py_modules=['flask_resteasy'],
+    # packages=find_packages(exclude=['tests']),
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
     install_requires=requirements,
     # Install these with "pip install -e '.[paging]'" or '.[docs]'
     # extras_require={
